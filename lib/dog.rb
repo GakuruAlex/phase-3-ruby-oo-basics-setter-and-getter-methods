bf243168acef5c79a21f9dd2fe0852dd824e3085
alexgicimo <gakurualexgicimo@gmail.com>
2022-10-18 18:40:29 +0300

    commit tree d378a5950ff62bcc54dc30aedd7226722b906f9a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
+#Dog name setter
+def name=(value)
+    @name=value
+end
+
+#Dog name getter
+def name
+    @name
+end
+
+#Dog breed setter
+def breed=(value)
+    @breed=value
+end
+
+#Dog breed getter
+def breed
+    @breed
+end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
 
+    def name=(name_value)
+        @name=name_value
+    end
+    
+    def name
+        @name
+    end
+
+    def job=(job_title)
+        @job=job_title
+    end
+
+    def job
+        @job
+    end
 end
